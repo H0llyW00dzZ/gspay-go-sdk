c41e3943768359fed3bf2815f57044202246840a
H0llyW00dzZ <h0llyw00dzz@pm.me>
2026-02-03 01:15:24 +0700

    Bump Version
    - [+] chore(constants): bump SDK version to 0.3.7

--- src/constants/version.go
+++ src/constants/version.go
@@ -25,7 +25,7 @@ const (
 
 	// SDKVersion is the current version of this SDK.
 	// This should be updated with each release.
-	SDKVersion = "0.3.6"
+	SDKVersion = "0.3.7"
 
 	// SDKRepository is the GitHub repository URL for this SDK.
 	SDKRepository = "https://github.com/H0llyW00dzZ/gspay-go-sdk"
